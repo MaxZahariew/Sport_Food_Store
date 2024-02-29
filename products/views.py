91c03fb7008678f3eb31b6f48304626111efa6e9
Maxim <maxim.zahariew@yandex.ru>
2024-02-29 15:07:18 +0700

    добавлены два шаблона в приложение products.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def catalog(request):
+    return render(request, 'products/catalog.html')
+
+
+def products(request):
+    return render(request, 'products/product.html')
